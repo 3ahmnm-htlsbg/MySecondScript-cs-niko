d39dae7cb917e263016329e6db34f61524571f9c
Niko1803 <nicolas.korb03@htl-salzburg.ac.at>
2019-11-05 13:48:26 +0100

    Debuging

--- Assets/HelloWorldHome.cs
+++ Assets/HelloWorldHome.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-     Debug.Log("Hello World at home")   
+      
 public class HelloWorldHome : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+Debug.Log("Hello World at home");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
 }
 
